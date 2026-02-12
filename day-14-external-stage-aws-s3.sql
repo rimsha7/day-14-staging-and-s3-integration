59bbd5633520ea7a1d2b9c5858a7ba636a3e4e9e
rimsha7 <72002316+rimsha7@users.noreply.github.com>
2026-02-12 13:33:51 +0500

    day-14-s3-integration

--- day-14-external-stage-aws-s3.sql
+++ day-14-external-stage-aws-s3.sql
@@ -1,1 +1,33 @@
--- TODO Again with proper authentication donot add Keys in COde
+!set variable_substitution=true;
+
+use database my_db;
+use schema my_schema;
+
+create or replace file format csv_format
+type = csv
+skip_header = 1
+field_delimiter = ','
+field_optionally_enclosed_by = '"'
+null_if = ('null','NULL');
+
+create or replace stage my_s3_stage
+url = 's3://day14-snowflake-bucket/sales.csv'
+credentials(
+    AWS_KEY_ID = '&AWS_KEY_ID'
+    AWS_SECRET_KEY = '&AWS_SECRET_KEY'
+)
+file_format= csv_format;
+
+list @my_s3_stage;
+
+create or replace table sales(
+    sale_date date,
+    product_category varchar,
+    sales_amount number(10,2)
+);
+
+copy into sales
+from @my_s3_stage;
+
+select * from sales
+limit 5;
